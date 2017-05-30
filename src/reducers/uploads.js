7438746865202ccffbeddf8dc12eb24adbf80c91
Brendan Wood <brendan@brendancwood.com>
2017-05-30 00:11:06 -0700

    last tweaks

--- src/reducers/uploads.js
+++ src/reducers/uploads.js
@@ -8,7 +8,7 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case types.UPLOAD_RESET:
+    case types.REQUEST_SEARCH:
       return initialState
     case types.UPLOADING_GIF:
       return {...state, status: UPLOAD_STATUS.PENDING}
